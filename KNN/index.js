168708f20f85b797988da83616c14190b692d1a8
AndreRdz7 <A01730716@itesm.mx>
2019-05-07 21:48:25 -0500

    knn function

--- KNN/index.js
+++ KNN/index.js
@@ -2,6 +2,22 @@ require("@tensorfow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
 
+function knn(features, labels, predictionPoint, k) {
+  return (
+    features
+      .sub(predictionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.get(0) > b.get(0) ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.get(1), 0) / k
+  );
+}
+
 let { features, labels, testFeatures, testLabels } = loadCSV(
   "kc_house_data.csv",
   {
@@ -12,15 +28,8 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   }
 );
 
-const k = 2;
-features
-  .sub(predictionPoint)
-  .pow(2)
-  .sum(1)
-  .pow(0.5)
-  .expandDims(1)
-  .concat(labels, 1)
-  .unstack()
-  .sort((a, b) => (a.get(0) > b.get(0) ? 1 : -1))
-  .slice(0, k)
-  .reduce((acc, pair) => acc + pair.get(1), 0) / k;
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+console.log("Guess: ", result, testLabels[0][0]);
